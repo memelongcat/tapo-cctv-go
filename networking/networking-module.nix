5dc39c07a69f04ad41d3ac461c92a5f6b125d8b7
memelongcat <krotovpeter87@outlook.com>
2024-11-13 22:27:29 +0700

    hostapd, dnsmasq, networking module config changes

--- networking/networking-module.nix
+++ networking/networking-module.nix
@@ -1,37 +1,51 @@
 { config,pkgs, ...}:
 
 {
-    networking = {
-        interfaces = {
-            wlo0 = {
-                ipv4 = {
-                    addresses = [{
-                        address = "192.168.50.1";
-                        prefixLength = 24;
-                    }];
-                    gateway = "192.168.50.1";
-                    useDHCP = false;
-                };
-            };
-            eno1 = {
-                    ipv4 = {
-                        address = [{
-                            address = "192.168.50.2";
-                            prefixLength = 24;
-                        }];
-                        gateway = "192.168.50.2";
-                        useDHCP = false;
-                    };
-                };
-        };
-        firewall = {
-            extraCommands = ''
-                iptables -t nat -A POSTROUTING -o enp0s20u2 -j MASQUERADE
-                iptables -A FORWARD -i enp0s20u2 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-                iptables -A FORWARD -i wlan0 -o enp0s20u2 -j ACCEPT
-                iptables -A FORWARD -i enp0s20u2 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-                iptables -A FORWARD -i eth0 -o enp0s20u2 -j ACCEPT
-            '';
-        };
-    };
+	boot.kernel.sysctl = { "net.ipv4.conf.all.forwarding" = true; };
+	networking = {
+		interfaces = {
+			wlo1 = {
+				useDHCP = false;
+				ipv4.addresses = [{
+					address = "192.168.50.1";
+					prefixLength = 24;
+				}];
+			};
+			eno1 = {
+				useDHCP = false;
+				ipv4.addresses = [{
+					address = "192.168.60.1";
+					prefixLength = 24;
+				}];
+			};
+			enp0s20u2 ={
+				useDHCP = true;
+			};
+		};
+#		nat = {
+#			enable = true;
+#			externalInterface = "enp0s20u2";
+#			internalInterfaces = [ "wlo1" "eno1" ];
+#		};
+#		defaultGateway = {
+#			address = "192.168.50.1";
+#			interface = "enp0s20u2";
+#		};
+		firewall = {
+			enable = true;
+			allowedTCPPorts = [ 53 80 443 ];
+			allowedUDPPorts = [ 53 67 68 ];
+			allowPing = true;
+
+			extraCommands = ''
+				iptables -t nat -A POSTROUTING -o enp0s20u2 -j MASQUERADE
+				iptables -A FORWARD -i wlo1 -o eno1 -j ACCEPT
+				iptables -A FORWARD -i eno1 -o wlo1 -j ACCEPT
+				iptables -A FORWARD -i eno1 -j LOG --log-prefix "FORWARD eno1: "
+				iptables -A FORWARD -i wlo1 -j LOG --log-prefix "FORWARD wlo1: "
+				iptables -A FORWARD -o enp0s20u2 -j LOG --log-prefix "FORWARD to enp0s20u2: "
+			'';
+		};
+		networkmanager.unmanaged = [ "wlo1" ];
+	};
 }
